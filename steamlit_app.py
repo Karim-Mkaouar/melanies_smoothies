0dc758e0480ee0d6eb400e3e4a469f5e960b15d9
Karim-Mkaouar <93012788+Karim-Mkaouar@users.noreply.github.com>
2026-02-13 16:48:54 +0100

    Refactor smoothie ordering logic and clean up code
    Removed commented-out code for displaying nutrition information and adjusted the placement of the API request.

--- steamlit_app.py
+++ steamlit_app.py
@@ -29,6 +29,8 @@ if ingredients_list:
     ingredients_string =''
     for fruit_choosen in ingredients_list:
         ingredients_string += fruit_choosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -42,8 +44,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered,'+' '+name_on_order+'!', icon="✅")
 
 
-# New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
